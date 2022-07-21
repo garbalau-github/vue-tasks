5cf950416b35d75710382eb4d4370e7269e8a721
Nick Garbalau <garbalaunick@protonmail.com>
2022-07-21 16:26:29 +0300

    Added computed property to render a button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close' : 'Add Task'"
       :color="showAddTask ? 'orangered' : 'rgb(21, 242, 21)'"
@@ -14,12 +15,21 @@ import Button from './Button.vue';
 
 export default {
   name: 'Header',
-  components: {
-    Button,
-  },
   props: {
     title: String,
     showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
   emits: ['toggle-add-task'],
 };
